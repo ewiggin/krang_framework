23c686b24649f05459ad8502fce3ea9127f11231
Mario Martinez <gameofender@gmail.com>
2015-06-13 21:42:52 +0200

    Delete confusion text

--- index.php
+++ index.php
@@ -11,10 +11,6 @@
  *  Aquesta estructura de carpetas s'aproxima als standards de programació Modelo Vista Controlador.
  * 
  * ==================================================º=====================================================
- *
- * Totes les vistes tenen disponible la variable $page. Aquesta variable te tota la informació sobre 
- * la pagina. Aquest sistema només es util quan les pagines venen de la base de dades. Quan es defineixen
- * a nivell de codi s'han de donar d'alta com apartats dins de config/apartats.php
  */
 
 
